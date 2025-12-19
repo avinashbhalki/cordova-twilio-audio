030ce9a96c19e5bfddd95563b45d49e6734e1ef1
Avinash Rao Bhalki <avinash207@gmail.com>
2025-12-19 10:33:03 +0530

    Update RetrofitAPi.java

--- src/android/RetrofitAPi.java
+++ src/android/RetrofitAPi.java
@@ -6,7 +6,6 @@ import com.google.gson.GsonBuilder;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitAPi {
@@ -28,7 +27,6 @@ public class RetrofitAPi {
                 .baseUrl("https://mhid.cloud9download.com:8080/")
                 .client(client)
                 .addConverterFactory(GsonConverterFactory.create(gson))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
     }
 
